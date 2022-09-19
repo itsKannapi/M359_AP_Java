714c5b3ce39e73e045672f642b33fee9d4350778
itsKannapi <112032889+itsKannapi@users.noreply.github.com>
2022-09-19 10:08:48 -0500

    Create Review.java
    classwork review

---
+++ M359_Semester1/src/Unit3/examples/Review.java
@@ -0,0 +1,17 @@
+package Unit3.examples;
+
+public class Review {
+    private String reviewerName;
+
+    private String businessName;
+
+    private int rating;
+
+    public Review(String reviewerName, String businessName, int rating) {
+        this.reviewerName = reviewerName;
+        this.businessName = businessName;
+        this.rating = rating;
+    }
+
+
+}
