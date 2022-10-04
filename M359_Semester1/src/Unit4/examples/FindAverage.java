8182cdec827014f05c221606db78542e0fff0b58
itsKannapi <112032889+itsKannapi@users.noreply.github.com>
2022-10-04 10:09:22 -0500

    added find average class
    classwork for 10/4/22

---
+++ M359_Semester1/src/Unit4/examples/FindAverage.java
@@ -0,0 +1,23 @@
+package Unit4.examples;
+
+import java.util.Scanner;
+
+public class FindAverage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        // user will enter -999 to exit
+        if (input.nextInt() == -999) {
+
+        }
+        // prompt the user for their value
+        // a while loop making sure they want to continue,
+        // add to the sum
+        sum += input.nextInt();
+        // increment the count
+
+        // calculate the average
+        // print it all out
+    }
+}
