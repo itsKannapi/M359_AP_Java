2c33e51765a24036121677a202796d58cf84c975
itsKannapi <112032889+itsKannapi@users.noreply.github.com>
2022-10-17 19:36:02 -0500

    pig latin updates
    homework 10/17

--- M359_Semester1/src/Unit4/PigLatinLab/PigLatin.java
+++ M359_Semester1/src/Unit4/PigLatinLab/PigLatin.java
@@ -3,7 +3,8 @@ package Unit4.PigLatinLab;
 public class PigLatin {
     public static String toPigLatin(String str){
         String newStr = "";
-        if (str.subString() == ("a")){
+
+        if (str.substring(0, 1) == "a"){
             for (int i = str.length() - 1; i > 0; i--){
                 newStr += str.charAt(i);
             }
@@ -14,7 +15,21 @@ public class PigLatin {
     }
     public static String translateWordToPigLatin(String str){
         String newStr = "";
-
+        // check if the str contains a vowel aeiou
+        if (str.substring(0, 1).equals("a")|| str.substring(0, 1).equals("e") || str.substring(0, 1).equals("i") ||
+        str.substring(0, 1).equals("o") || str.substring(0, 1).equals("u")){
+            for (int i = str.length() - 1; i > 1; i--){
+                newStr += str.charAt(i);
+            }
+            newStr += "yay";
+        }
+        else{
+            for (int j = str.length() - 1; j > 1; j--){
+                newStr += str.charAt(j);
+            }
+            newStr += "ay";
+        }
+        // check if the str contains the consonant or consanant cluster
         return newStr;
     }
 }
