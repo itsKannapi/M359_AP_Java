c88abce65d21a94c600f9455c226ae489658d1fd
itsKannapi <112032889+itsKannapi@users.noreply.github.com>
2022-10-19 23:55:18 -0500

    completed piglatin word by word

--- M359_Semester1/src/Unit4/PigLatinLab/PigLatin.java
+++ M359_Semester1/src/Unit4/PigLatinLab/PigLatin.java
@@ -1,35 +1,65 @@
 package Unit4.PigLatinLab;
 
+import java.util.Locale;
+
 public class PigLatin {
     public static String toPigLatin(String str){
         String newStr = "";
+        str = str.toLowerCase();
 
-        if (str.substring(0, 1) == "a"){
-            for (int i = str.length() - 1; i > 0; i--){
-                newStr += str.charAt(i);
+        for (int k = 1; k < str.length(); k++){
+            while(str.charAt(k) != ' '){
+                newStr += translateWordToPigLatin(str);
             }
         }
+
 
 
         return newStr;
     }
-    public static String translateWordToPigLatin(String str){
+    public static String translateWordToPigLatin(String s){
         String newStr = "";
         // check if the str contains a vowel aeiou
-        if (str.substring(0, 1).equals("a")|| str.substring(0, 1).equals("e") || str.substring(0, 1).equals("i") ||
-        str.substring(0, 1).equals("o") || str.substring(0, 1).equals("u")){
-            for (int i = str.length() - 1; i > 1; i--){
-                newStr += str.charAt(i);
+        if (checkForVowel(s, 0)){
+            for (int i = 1; i < s.length(); i++){
+                newStr += s.charAt(i);
             }
             newStr += "yay";
         }
-        else{
-            for (int j = str.length() - 1; j > 1; j--){
-                newStr += str.charAt(j);
+        // checks if there's consonant cluster
+        else if (checkForVowel(s, 1)){
+            for (int i = 2; i < s.length(); i++){
+                newStr += s.charAt(i);
             }
             newStr += "ay";
         }
-        // check if the str contains the consonant or consanant cluster
+        // returns as normal w/ normal consonants
+        else{
+            for (int j = 1; j < s.length(); j++){
+                newStr += s.charAt(j);
+            }
+            newStr += "ay";
+        }
         return newStr;
+    }
+    // create a helper method that checks if the word starts with a vowel
+    //
+    public static boolean checkForVowel(String str, int index){
+        if (str.charAt(index) == 'a' || str.charAt(index) == 'e' || str.charAt(index) == 'i'
+                || str.charAt(index) == 'o' || str.charAt(index) == 'u'){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    // create a helper method that checks if there's a space
+    public static boolean checkForSpace(String str, int index){
+        if (str.charAt(index) == ' '){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
