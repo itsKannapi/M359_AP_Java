723252bb5c69119a2c43e1ff85fb3de99c04524e
itsKannapi <112032889+itsKannapi@users.noreply.github.com>
2022-12-01 10:08:20 -0600

    classwork for 12/1

--- M359_Semester1/src/Unit6/CourseStudentExample/StudentTester.java
+++ M359_Semester1/src/Unit6/CourseStudentExample/StudentTester.java
@@ -6,64 +6,75 @@ import java.util.Scanner;
 
 public class StudentTester {
     public static void main(String[] args) throws FileNotFoundException {
-        Course per1 = new Course("Anderson","AP Literature", "A-", 1);
-        Course per2 = new Course("Ganas","AP Psychology", "B", 2);
-        Course per3 = new Course("Denna","Ap Comp Sci A.", "B", 3);
-        Course per4 = new Course("Lunch", 4);
-        Course per5 = new Course("Kent","Intro to Econ.", "B", 5);
-        Course per6 = new Course("Brandenburg","Strength & Con", "A", 6);
-        Course per7 = new Course("Hochstetter","Calc for Bus & Soc Sci", "A-", 7);
-        Course per8 = new Course("Excused", 8);
-
-        Course[] courses = {per1, per2, per3, per4, per5, per6, per7, per8};
-
-        Student Joey = new Student("Joey",courses);
+//        Course per1 = new Course("Anderson","AP Literature", "A-", 1);
+//        Course per2 = new Course("Ganas","AP Psychology", "B", 2);
+//        Course per3 = new Course("Denna","Ap Comp Sci A.", "B", 3);
+//        Course per4 = new Course("Lunch", 4);
+//        Course per5 = new Course("Kent","Intro to Econ.", "B", 5);
+//        Course per6 = new Course("Brandenburg","Strength & Con", "A", 6);
+//        Course per7 = new Course("Hochstetter","Calc for Bus & Soc Sci", "A-", 7);
+//        Course per8 = new Course("Excused", 8);
+//
+//        Course[] courses = {per1, per2, per3, per4, per5, per6, per7, per8};
+//
+//        Student Joey = new Student("Joey",courses);
         //System.out.println(Joey);
 
 
 
 
-        //Create a new file variable to open the file
-        File oneCourse = new File("oneCourse.txt");
+//        //Create a new file variable to open the file
+//        File oneCourse = new File("oneCourse.txt");
+//
+//        //Create a file and scanner variable
+//        File oneSch = new File("oneSchedule.txt");
+//        Scanner inSch = new Scanner(oneSch);
+//        //Read in student name
+//        String name = inSch.nextLine();
+//        //Create a Course[] and instantiate it to be of size 8
+//        Course[] allClasses = new Course[8];
+//        //use a  for loop to read in data for a Course 8 times
+//        for (int i = 0; i < allClasses.length; i++){
+//            //Read in teacher name, Course, grade, period
+//            String teacherName = inSch.nextLine();
+//            String className = inSch.nextLine();
+//            String grade = inSch.nextLine();
+//            int per = inSch.nextInt(); // only reads the int, does not go to the next line
+//            // need a dummy read to clear out the EOL character
+//            if (inSch.hasNextLine()){
+//                inSch.nextLine();
+//            }
+//
+//            //Create a Course var with this data
+//            Course c = new Course(teacherName, className, grade, per);
+//            //Save this course in the Course array
+//            allClasses[i] = c;
+//        }
+//        //create a student object with his name and Course[]
+//
+//        //print the student to see if it worked
+//
+//        //Create a Scanner to read data from the file
+//        // Select the option to add the throws FileNotFoundException to the main header
+//        Scanner inF = new Scanner(oneCourse);
+//
+//        //now we can call methods on the scanner object inF to read data
+//        // .next() .nextLine() .nextInt() .hasNextLine() etc.
+//
+//        String teacherName = inF.nextLine();
+//        String className = inF.nextLine(); //reads the ENTIRE LINE UNTIL THE END OF LINE CHAR
+//        String grade = inF.nextLine();
+//        int per = inF.nextInt();
+//
+//        //Create a course var with this data
+//        Course c = new Course(teacherName, className, grade, per);
+//        System.out.println(c);
+//
+//        Student s = new Student(name, allClasses);
+//        System.out.println(s);
 
-        //Create a file and scanner variable
-        File oneSch = new File("oneSchedule.txt");
-        Scanner inSch = new Scanner(oneSch);
-        //Read in student name
-        String name = inSch.nextLine();
-        //Create a Course[] and instantiate it to be of size 8
-        Course[] allClasses = new Course[8];
-        //use a  for loop to read in data for a Course 8 times
-        for (int i = 0; i < allClasses.length; i++){
-            //Read in teacher name, Course, grade, period
-            String teacherName = inSch.nextLine();
-            String className = inSch.nextLine();
-            String grade = inSch.nextLine();
-            int per = inSch.nextInt();
+        //create a file variable to open the scheduledata text
+        File studentScheduleData = new File("studentScheduleData.txt");
 
-            //Create a Course var with this data
-            Course c = new Course(teacherName, className, grade, per);
-            //Save this course in the Course array
-            allClasses[i] = c;
-        }
-        //create a student object with his name and Course[]
-        
-        //print the student to see if it worked
-
-        //Create a Scanner to read data from the file
-        // Select the option to add the throws FileNotFoundException to the main header
-        Scanner inF = new Scanner(oneCourse);
-
-        //now we can call methods on the scanner object inF to read data
-        // .next() .nextLine() .nextInt() .hasNextLine() etc.
-
-        String teacherName = inF.nextLine();
-        String className = inF.nextLine(); //reads the ENTIRE LINE UNTIL THE END OF LINE CHAR
-        String grade = inF.nextLine();
-        int per = inF.nextInt();
-
-        //Create a course var with this data
-        Course c = new Course(teacherName, className, grade, per);
-        System.out.println();
     }
 }
