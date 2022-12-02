3e50a78cf0700b97cdbf836b8810be269f0f644b
itsKannapi <112032889+itsKannapi@users.noreply.github.com>
2022-12-02 10:09:45 -0600

    classwork 12/2/22

--- M359_Semester1/src/Unit6/CourseStudentExample/StudentTester.java
+++ M359_Semester1/src/Unit6/CourseStudentExample/StudentTester.java
@@ -18,17 +18,49 @@ public class StudentTester {
 //        Course[] courses = {per1, per2, per3, per4, per5, per6, per7, per8};
 //
 //        Student Joey = new Student("Joey",courses);
-        //System.out.println(Joey);
+//        System.out.println(Joey);
 
 
 
 
-//        //Create a new file variable to open the file
-//        File oneCourse = new File("oneCourse.txt");
-//
-//        //Create a file and scanner variable
-//        File oneSch = new File("oneSchedule.txt");
-//        Scanner inSch = new Scanner(oneSch);
+        //Create a new file variable to open the file
+        File file = new File("studentScheduleData.txt");
+        Scanner inF = new Scanner(file);
+        int numStudents = inF.nextInt();
+        inF.nextLine();
+
+        Student[] students = new Student[numStudents];
+        int currStu = 0;
+
+        while(inF.hasNextLine()){
+            String name = inF.nextLine();
+            Course[] allClasses = new Course[8];
+            //use a  for loop to read in data for a Course 8 times
+            for (int i = 0; i < allClasses.length; i++){
+                //Read in teacher name, Course, grade, period
+                String teacherName = inF.nextLine();
+                String className = inF.nextLine();
+                String grade = inF.nextLine();
+                int per = inF.nextInt(); // only reads the int, does not go to the next line
+                // need a dummy read to clear out the EOL character
+                if (inF.hasNextLine()){
+                    inF.nextLine();
+                }
+
+                //Create a Course var with this data
+                Course c = new Course(teacherName, className, grade, per);
+                //Save this course in the Course array
+                allClasses[i] = c;
+            }
+            Student s = new Student(name, allClasses);
+            students[currStu] = s;
+            currStu++;
+        }
+        for (Student s: students) {
+            if (s != null){
+                System.out.println(s);
+            }
+        }
 //        //Read in student name
 //        String name = inSch.nextLine();
 //        //Create a Course[] and instantiate it to be of size 8
@@ -65,7 +97,7 @@ public class StudentTester {
 //        String className = inF.nextLine(); //reads the ENTIRE LINE UNTIL THE END OF LINE CHAR
 //        String grade = inF.nextLine();
 //        int per = inF.nextInt();
-//
+
 //        //Create a course var with this data
 //        Course c = new Course(teacherName, className, grade, per);
 //        System.out.println(c);
@@ -73,8 +105,7 @@ public class StudentTester {
 //        Student s = new Student(name, allClasses);
 //        System.out.println(s);
 
-        //create a file variable to open the scheduledata text
-        File studentScheduleData = new File("studentScheduleData.txt");
+
 
     }
 }
