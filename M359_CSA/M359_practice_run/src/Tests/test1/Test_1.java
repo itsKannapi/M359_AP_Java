e0647c65ccadbd009d407b78c8f913d47a758483
Kannapi <112032889+itsKannapi@users.noreply.github.com>
2023-05-04 10:03:19 -0500

    final structuring of AP CSA folder
    this is an indication of completion of AP CSA and the end of my high school career :D

--- M359_CSA/M359_practice_run/src/Tests/test1/Test_1.java
+++
@@ -1,6 +0,0 @@
-package Tests.test1;
-
-public class Test_1 {
-    public static void main(String[] args) {
-        System.out.print("Joey Tamondong \tPalatine \tBlue \tI like video games \tI had corned beef and rice"); }
-}
--- M359_CSA/M359_practice_run/src/quiz_/quiz1.java
+++
@@ -1,8 +0,0 @@
-package quiz_;
-
-public class quiz1 {
-    public static void main(String[] args) {
-        System.out.println("\\\\ || // \n\t **** \n\t &&");
-        System.out.print("\"Java\" \n Don't give up!");
-    };
-}
